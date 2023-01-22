d29e498df4f4e304f62a0a70f9779cfbdbf6a2b6
IgorD934 <igord934@gmail.com>
2023-01-22 08:41:26 -0300

    Integração com a API, criação de habitos, checkbox e validação de formularios

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,14 +1,22 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import * as Popover from "@radix-ui/react-popover";
 import clsx from "clsx";
+import dayjs from "dayjs";
+import { Check } from "phosphor-react";
 import { ProgressBar } from "./ProgressBar";
 
 interface iHabitProps {
-  completed: number;
-  amount: number;
+  date: Date;
+  completed?: number;
+  amount?: number;
 }
 
-export function HabitDay(props: iHabitProps) {
-  const progress = Math.round((props.completed * 100) / props.amount);
+export function HabitDay({ completed = 0, amount = 0, date }: iHabitProps) {
+  const progress = amount > 0 ? Math.round((completed * 100) / amount) : 0;
+
+  const dayAndMonth = dayjs(date).format("DD/MM");
+  const dayOfWeek = dayjs(date).format("dddd");
+
   return (
     <Popover.Root>
       <Popover.Trigger
@@ -23,11 +31,27 @@ export function HabitDay(props: iHabitProps) {
       />
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-          <span className="font-semibold text-zinc-400">Sabado</span>
+          <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">
-            21/01
+            {dayAndMonth}
           </span>
+
           <ProgressBar progress={progress} />
+
+          <div className="mt-6 flex flex-col gap-6">
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                Beber 2L de agua
+              </span>
+            </Checkbox.Root>
+          </div>
+
           <div className="h"></div>
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,12 +1,28 @@
 import { generateDatesFromYearsBeginning } from "../utils/generate-dates-from-years-beginning";
 import { HabitDay } from "./HabitDay";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
+import dayjs from "dayjs";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 const summaryDates = generateDatesFromYearsBeginning();
 const minimunSummaryDatesSize = 18 * 7;
 const amountOfDaysToFill = minimunSummaryDatesSize - summaryDates.length;
+
+type tSummary = {
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}[];
+
 export function SummaryTable() {
+  const [summary, setSummary] = useState<tSummary>([]);
+
+  useEffect(() => {
+    api.get("/summary").then((res) => setSummary(res.data));
+  }, []);
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -22,13 +38,21 @@ export function SummaryTable() {
         })}
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((summaryDay) => (
-          <HabitDay
-            key={summaryDay.toString()}
-            amount={5}
-            completed={Math.round(Math.random() * 5)}
-          />
-        ))}
+        {summaryDates.map((summaryDay) => {
+          const dayInSumary = summary.find((day) =>
+            dayjs(summaryDay).isSame(day.date, "day")
+          );
+
+          return (
+            <HabitDay
+              key={summaryDay.toString()}
+              date={summaryDay}
+              amount={dayInSumary?.amount}
+              completed={dayInSumary?.completed}
+            />
+          );
+        })}
+
         {amountOfDaysToFill > 0 &&
           Array.from({ length: amountOfDaysToFill }).map((v, i) => {
             return (
